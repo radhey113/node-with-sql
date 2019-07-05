d799f28c65ef406eec6faafe99cca6980bd748e2
Manish Gulati <mgulati@chicmic.in>
2019-07-05 15:53:05 +0530

    Read me updated

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ require('dotenv').config();
 /** to run server **/
 require('./bin/server.js')();
 
+// https://medium.com/@prajramesh93/getting-started-with-node-express-and-mysql-using-sequelize-ed1225afc3e0
